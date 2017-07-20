4a4b4be2ba7a2fddd76202a564e7ad0b28db10ba
alex <oleksandr.yeropudov@gmail.com>
2017-07-20 17:50:13 +0300

    change parser step 1

--- parse.py
+++ parse.py
@@ -50,6 +50,22 @@ def parse_html(uri):
     price_str = re_w.sub('', price_str)
     sqls.append((uri[1], u"{}".format(description), price_str))
 
+# Парстинг HTML соохранение результатов
+def parse_html2(uri):
+    art = uri[0]
+    text = read_file(u"/home/alex/spider/html/{}.html".format(art))
+    root = html.fromstring(text)
+    results = root.xpath('//div[@class = "eItemProperties_text"]/text()')
+    price_div = root.xpath('//div[@class="bSaleColumn"]/span/text()')
+    description = ''
+    price_str = ''
+    for res in results:
+        description = description + res
+    for price in price_div:
+        price_str = price_str + price
+    re_w = re.compile(' ')
+    price_str = re_w.sub('', price_str)
+    sqls.append((uri[1], u"{}".format(description), price_str))
 
 # очередь
 def task_queue(task, iterator, pool):
@@ -74,7 +90,11 @@ http = urllib3.PoolManager(10, headers=None, timeout=5, retries=1)
 #executor = futures.ThreadPoolExecutor(max_workers=10)
 #results_www = task_queue(parse_www, arts_unique, executor)
 executor = futures.ThreadPoolExecutor(max_workers=1)
-results_html = task_queue(parse_html, arts_unique, executor)
+#results_html = task_queue(parse_html2, arts_unique, executor)
+#executor = futures.ThreadPoolExecutor(max_workers=2)
+#results_html2 = task_queue(parse_html, arts_unique, executor)
+#executor = futures.ThreadPoolExecutor(max_workers=4)
+results_html3 = task_queue(parse_html, arts_unique, executor)
 xls_reader.put_stat_toxls(sqls)
 #root = html.fromstring(html_string)
 #results = root.xpath('//div[@class = "eItemProperties_text"]/text()')
